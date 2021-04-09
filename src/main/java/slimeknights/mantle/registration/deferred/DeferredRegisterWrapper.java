4a28d199abd481dba69e40888b530d2deef65605
hYdos <haydenv06@gmail.com>
2021-04-09 20:28:29 +1000

    im going to kill whoever thought enums are a good idea

--- src/main/java/slimeknights/mantle/registration/deferred/DeferredRegisterWrapper.java
+++ src/main/java/slimeknights/mantle/registration/deferred/DeferredRegisterWrapper.java
@@ -1,9 +1,11 @@
 package slimeknights.mantle.registration.deferred;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.StringIdentifiable;
 import slimeknights.mantle.registration.object.EnumObject;
+import slimeknights.mantle.registration.object.ItemEnumObject;
 
 import java.util.function.BiFunction;
 import java.util.function.Supplier;
@@ -57,6 +59,7 @@ public abstract class DeferredRegisterWrapper{
     return builder.build();
   }
 
+
   /**
    * Registers an item with multiple variants, suffixing the name with the value name
    * @param name      Name of the block
@@ -70,6 +73,45 @@ public abstract class DeferredRegisterWrapper{
     }
     // note this cast only works because you cannot extend an enum
     EnumObject.Builder<E,V> builder = new EnumObject.Builder<>(values[0].getDeclaringClass());
+    for (E value : values) {
+      builder.put(value, register.apply(name + "_" + value.asString(), value));
+    }
+    return builder.build();
+  }
+
+  /**
+   * Registers an item with multiple variants, prefixing the name with the value name
+   * @param values    Enum values to use for this block
+   * @param name      Name of the block
+   * @param register  Function to register an entry
+   * @return  EnumObject mapping between different block types
+   */
+  protected static <E extends Enum<E> & StringIdentifiable, V extends T, T extends Item> ItemEnumObject<E,V> registerItemEnum(E[] values, String name, BiFunction<String,E, Supplier<? extends V>> register) {
+    if (values.length == 0) {
+      throw new IllegalArgumentException("Must have at least one value");
+    }
+    // note this cast only works because you cannot extend an enum
+    ItemEnumObject.Builder<E,V> builder = new ItemEnumObject.Builder<>(values[0].getDeclaringClass());
+    for (E value : values) {
+      builder.put(value, register.apply(value.asString() + "_" + name, value));
+    }
+    return builder.build();
+  }
+
+
+  /**
+   * Registers an item with multiple variants, suffixing the name with the value name
+   * @param name      Name of the block
+   * @param values    Enum values to use for this block
+   * @param register  Function to register an entry
+   * @return  EnumObject mapping between different block types
+   */
+  protected static <E extends Enum<E> & StringIdentifiable, V extends Item> ItemEnumObject<E,V> registerItemEnum(String name, E[] values, BiFunction<String,E,Supplier<? extends V>> register) {
+    if (values.length == 0) {
+      throw new IllegalArgumentException("Must have at least one value");
+    }
+    // note this cast only works because you cannot extend an enum
+    ItemEnumObject.Builder<E,V> builder = new ItemEnumObject.Builder<>(values[0].getDeclaringClass());
     for (E value : values) {
       builder.put(value, register.apply(name + "_" + value.asString(), value));
     }
--- src/main/java/slimeknights/mantle/registration/deferred/ItemDeferredRegister.java
+++ src/main/java/slimeknights/mantle/registration/deferred/ItemDeferredRegister.java
@@ -1,9 +1,13 @@
 package slimeknights.mantle.registration.deferred;
 
 import net.minecraft.item.Item;
+import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.registry.Registry;
+import slimeknights.mantle.registration.object.EnumObject;
+import slimeknights.mantle.registration.object.ItemEnumObject;
 import slimeknights.mantle.registration.object.ItemObject;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -34,5 +38,27 @@ public class ItemDeferredRegister extends DeferredRegisterWrapper {
    */
   public ItemObject<Item> register(String name, Item.Settings props) {
     return register(name, () -> new Item(props));
+  }
+
+  /**
+   * Registers an item with multiple variants, prefixing the name with the value name
+   * @param values   Enum values to use for this item
+   * @param name     Name of the block
+   * @param mapper   Function to get a item for the given enum value
+   * @return  EnumObject mapping between different item types
+   */
+  public <T extends Enum<T> & StringIdentifiable, I extends Item> ItemEnumObject<T,I> registerEnum(T[] values, String name, Function<T,? extends I> mapper) {
+    return registerEnum(values, name, (fullName, type) -> register(fullName, () -> mapper.apply(type)));
+  }
+
+  /**
+   * Registers an item with multiple variants, suffixing the name with the value name
+   * @param values   Enum values to use for this item
+   * @param name     Name of the block
+   * @param mapper   Function to get a item for the given enum value
+   * @return  EnumObject mapping between different item types
+   */
+  public <T extends Enum<T> & StringIdentifiable, I extends Item> ItemEnumObject<T,I> registerEnum(String name, T[] values, Function<T,? extends I> mapper) {
+    return registerEnum(name, values, (fullName, type) -> register(fullName, () -> mapper.apply(type)));
   }
 }
